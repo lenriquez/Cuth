db8cdea5b8a86944160057d3c62ef1cc815058d4
Luis Felipe Enriquez <lenriquez@alertlogic.com>
2016-11-02 06:30:24 -0500

    Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'auth0/callback'
-
-  get 'auth0/failure'
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
