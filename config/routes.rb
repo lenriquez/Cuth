ff5e0b75c2c2e23b9614c5d8ba13a62ec27556af
Luis Felipe Enriquez <lenriquez@alertlogic.com>
2016-11-02 06:22:13 -0500

    Adding controllers with command: rails generate controller auth0 callback failure --skip-template-engine --skip-assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'auth0/callback'
+
+  get 'auth0/failure'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
     scope '/v1' do
